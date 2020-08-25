b68084a5b9797f6edf56d5f682b6105138a1dc47
Exso Kamabay <45049739+ExsoKamabay@users.noreply.github.com>
2020-08-25 19:14:57 +0800

    Update main.py

--- slideTop/main.py
+++ slideTop/main.py
@@ -4,5 +4,5 @@ class Api_js:
 if __name__=="__main__":
     kmy = Api_js();
     win = webview.create_window("KAMABAY",
-    html=html,min_size=(900,650));
+    html=html,js_api=kmy,min_size=(900,650));
     webview.start(loadCSS,win); 
